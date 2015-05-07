8fb3dcdee5e83a890315f04058e48ae64aed55f9
ajit.kumar <ajit.kumar@tpvision.com>
2015-05-07 20:03:38 +0530

    My first Commit

---
+++ MyMom/app/src/main/java/com/tpvision/ajitkumar/mymom/MyActivity.java
@@ -0,0 +1,62 @@
+package com.tpvision.ajitkumar.mymom;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+
+public class MyActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.tpvision.ajitkumar.mymom";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        switch (id) {
+            case R.id.action_search:
+               // openSearch();
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    public void SendMessage(View view) {
+
+        Intent intent = new Intent(this , DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString() ;
+        intent.putExtra(EXTRA_MESSAGE , message );
+        startActivity(intent);
+
+    }
+}
